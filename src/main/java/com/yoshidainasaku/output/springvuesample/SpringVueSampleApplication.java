9300991d132291cba5aa77a7127c25431204cf88
吉田稲作 <yoshidanoinasaku@gmail.com>
2023-06-07 08:27:54 +0900

    git管理外ファイルになっていたものをgit管理対象に追加

---
+++ src/main/java/com/yoshidainasaku/output/springvuesample/SpringVueSampleApplication.java
@@ -0,0 +1,13 @@
+package com.yoshidainasaku.output.springvuesample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringVueSampleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringVueSampleApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/yoshidainasaku/output/springvuesample/SpringVueSampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yoshidainasaku.output.springvuesample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringVueSampleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
